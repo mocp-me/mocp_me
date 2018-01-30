cde4cd069c1977718c99982caa76835da77f944c
Lane Anderson <laneanderson92@gmail.com>
2018-01-30 11:32:31 -0600

    updated routes to use env_variable

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Routes
-app.use(express.static("client/build"));
+app.use('/static', express.static(path.join(__dirname, 'client/build')));
 app.use("/", routes);
 
 // Activate express server
