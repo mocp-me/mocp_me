ba9240495c7b0b81c6965c41439fb8d1eccc211e
Dennis Hodges <fermentationist@gmail.com>
2018-02-07 19:03:17 -0600

    changed BrowserRouter back to Router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 // Normalizes CSS defaults in varying browsers
 import { Normalize } from 'normalize-css';
 
