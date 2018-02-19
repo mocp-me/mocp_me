009787dd1b6161f958a91d41d2f517e005d2ee0a
Amanda Yamasaki <amandayamasaki@u.northwestern.edu>
2018-02-19 15:06:30 -0600

    updated landing

--- client/src/components/nav_panel/nav_panel.js
+++ client/src/components/nav_panel/nav_panel.js
@@ -28,13 +28,16 @@ class NavPanel extends Component {
 		    	<Col xs={12} className={animationClass}>
 		    		<div className="frame">
 		    			<div className="extra_1"></div>
-		    			<div className="extra_2"></div>
+		    			<div className="extra_2">
+		    				<div className="extra_2_2"></div>
+		    			</div>
 				    	<div className="textWrapper">
 					        <p className="textStyle"><b>{this.props.text1}</b></p>
 					        <p className="textStyle"><b>{this.props.text2}</b></p>
 					        <p className="textStyle"><b>{this.props.text3}</b></p>
 						</div>
 						<div className="extra_3"></div>
+						<div className="extra_4"></div>
 					</div>
 			    </Col>
 		    </Row>
