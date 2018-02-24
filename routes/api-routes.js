5d54b39b9e7148f8776216eb2d4cefbb3bb4d91c
mkdoh3 <mkdoherty3@GMAIL.COM>
2018-02-23 21:59:50 -0600

    commit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -82,7 +82,7 @@ const apiRoutes = (() => {
 	router.post('/submit-tag', (req, res) => {
 		console.log("route hit: ", req.body)
 		const { id, tag } = req.body
-		db.user_tags.findOrCreate({
+		db.UserTags.findOrCreate({
 			where: {
 				tag_name: tag,
 				photo_id: id
